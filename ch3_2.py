7037246853c2a482f4094d339a7e88cc738e3c6e
liutong <jackie6053@outlook.com>
2019-01-14 14:45:31 +0800

    init project

---
+++ ch3_2.py
@@ -0,0 +1,9 @@
+motorcycles = ['honda', 'yamaha', 'suzuki', '123', '3435']
+
+#删除语句删除元素
+del motorcycles[0]
+print(motorcycles)
+
+# pop方式删除
+motorcycles.pop(1)
+print(motorcycles)
---
+++ ch3_2_exercises.py
@@ -0,0 +1,26 @@
+guests = ["zhangsan", "lisi", "wangwu"]
+print(guests)
+
+up_index = 1
+no_guest = guests.pop(up_index)
+print(no_guest + " can't go to the party")
+
+guests.insert(up_index, "zhaoliu")
+print(guests[up_index] + " will joined the party")
+
+print("the new list is " + str(guests))
+
+print("\r\nGood news: we find a bigger dining-table")
+guests.insert(0, "laoqi")
+guests.insert(2, "laoba")
+guests.append("laojiu")
+print("the three list is " + str(guests))
+
+print("\rI`m so sorry, the new dining-table cannot be served on time,  We can only invite two \r")
+del_guest = guests.pop()
+print(del_guest + "will be cancel the invitation \r")
+del_guest = guests.pop(-2)
+print(del_guest + "will be cancel the invitation \r")
+del_guest = guests.pop(-3)
+print(del_guest + "will be cancel the invitation \r")
+
