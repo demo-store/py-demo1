c33ca8903ef0d7364e20f0550be31af4c76efeb9
liutong <jackie6053@outlook.com>
2019-01-16 16:11:18 +0800

    update package

---
+++ ch4/magicians.py
@@ -0,0 +1,5 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a greet trick")
+    print("I cant`t wait to see your next trick, " + magician.title() + "\n")
+print("Thank you, everyone. That was a greet magic show!")
