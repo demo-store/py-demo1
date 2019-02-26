b39a8c0721cdaa787b7fdb8e8fcae2d056920aaa
liutong <jackie6053@outlook.com>
2019-02-26 11:19:41 +0800

    add ch6

---
+++ ch6/ch6_2_6.py
@@ -0,0 +1,10 @@
+alien_0 = {"color": "green", "points": 3}
+print(alien_0)
+
+alien_1 = {
+    "zhangsan": "c",
+    "lisi": "java",
+    "wangwu": "net",
+    "who": "python"
+}
+print(alien_1['zhangsan'])
