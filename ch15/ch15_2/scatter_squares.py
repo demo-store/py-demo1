2240a6094a984a6939d6f9cce60f3f769c6843ab
liutong <jackie6053@outlook.com>
2019-03-17 16:06:29 +0800

    15.2.9

--- ch15/ch15_2/scatter_squares.py
+++ ch15/ch15_2/scatter_squares.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
-plt.scatter(x_values, y_values, s=100)
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolors='none', s=40)
 
 plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
@@ -11,4 +12,10 @@ plt.ylabel("Square of Value", fontsize=14)
 # 设置刻度标记的大小
 plt.tick_params(axis='both', which='major', labelsize=14)
 
+plt.axis([0, 1100, 0, 1100000])
+
+# 显示图表
 plt.show()
+
+# 保存图表
+plt.savefig('squares_plot.png', bbox_inches='tight')
