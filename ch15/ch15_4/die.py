69b75b51d19dd8fe587a728b5e6c6fccada74654
liutong <jackie6053@outlook.com>
2019-03-20 17:49:35 +0800

    update d6 d6

--- ch15/ch15_4/die.py
+++ ch15/ch15_4/die.py
@@ -4,7 +4,7 @@ from random import randint
 class Die:
     """表示一个骰子的类"""
 
-    def ___init__(self, num_sides=6):
+    def __init__(self, num_sides=6):
         """骰子默认为6面"""
         self.num_sides = num_sides
 
--- ch15/ch15_4/die_visual.py
+++ ch15/ch15_4/die_visual.py
@@ -1,11 +1,31 @@
+import pygal
+
 from die import Die
 
 # 创建一个D6
-die6 = Die()
+die61 = Die()
+die62 = Die()
 
 results = []
-for roll_num in range(10):
-    result = die6.roll()
+for roll_num in range(1000):
+    result = die61.roll() + die62.roll()
     results.append(result)
 
-print(results)
+# 分析结果
+frequencies = []
+size = die61.num_sides + die62.num_sides
+for value in range(2, size + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# 对结果进行可视化
+hist = pygal.Bar()
+
+hist.title = "Result of rolling one D6 1000 times"
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6+D6', frequencies)
+hist.render_to_file("die_visual_d2.svg")
+
