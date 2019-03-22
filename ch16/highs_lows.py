c5b265d33a1261c594c1bd380c3802364e7e4713
liutong <jackie6053@outlook.com>
2019-03-22 16:17:34 +0800

    update 16.1.2

--- ch16/highs_lows.py
+++ ch16/highs_lows.py
@@ -4,4 +4,7 @@ filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+
+    # 对列表调用了enumerate()来获取每个元素的索引及其值。
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
