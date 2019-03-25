eef10432380b2574189fd68df52856c23706888b
jackie6053@outlook.com <liuTong131128>
2019-03-25 22:05:29 +0800

    up 16.1.4

--- ch16/highs_lows.py
+++ ch16/highs_lows.py
@@ -4,4 +4,11 @@ filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+
+    highs = []
+    for row in reader:
+        high = int(row[1])
+        highs.append(high)
+
+    print(highs)
+
