bdf5a48b51c0a3111f8eb6f2de2f088bd28b5f09
jackie6053@outlook.com <liuTong131128>
2019-03-27 22:55:21 +0800

    up 16.1.8

--- ch16/highs_lows.py
+++ ch16/highs_lows.py
@@ -8,7 +8,7 @@ with open(filename) as f:
     header_row = next(reader)
 
     # 从文件钟获取最高气温
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
@@ -16,12 +16,16 @@ with open(filename) as f:
         high = int(row[1])
         highs.append(high)
 
+        low = int(row[3])
+        lows.append(low)
+
     # 根据数据绘制图形
     fig = plt.figure(dpi=128, figsize=(10, 6))
     plt.plot(dates, highs, c='red')
+    plt.plot(dates, lows, c='blue')
 
     # 设置图形的格式
-    plt.title("Daily high temperatures - 2014", fontsize=24)
+    plt.title("Daily high and low temperatures - 2014", fontsize=24)
     plt.xlabel("", fontsize=16)
     fig.autofmt_xdate()
     plt.ylabel("Temperature (F)", fontsize=16)
