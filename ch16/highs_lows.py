038bc9bd29b537e89ff15c7fad2d0d9c1183c122
jackie6053@outlook.com <liuTong131128>
2019-03-27 22:38:42 +0800

    up 16.1.6

--- ch16/highs_lows.py
+++ ch16/highs_lows.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 from matplotlib import pyplot as plt
 
 filename = 'sitka_weather_07-2014.csv'
@@ -7,18 +8,22 @@ with open(filename) as f:
     header_row = next(reader)
 
     # 从文件钟获取最高气温
-    highs = []
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
+
         high = int(row[1])
         highs.append(high)
 
     # 根据数据绘制图形
     fig = plt.figure(dpi=128, figsize=(10, 6))
-    plt.plot(highs, c='red')
+    plt.plot(dates, highs, c='red')
 
     # 设置图形的格式
     plt.title("Daily high temperatures", fontsize=24)
     plt.xlabel("", fontsize=16)
+    fig.autofmt_xdate()
     plt.ylabel("Temperature (F)", fontsize=16)
     plt.tick_params(axis='both', which='major', labelsize=16)
 
