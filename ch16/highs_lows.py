c64a45c2583108502525bd2ff92d115e55805113
jackie6053@outlook.com <liuTong131128>
2019-03-27 22:52:45 +0800

    up 16.1.7

--- ch16/highs_lows.py
+++ ch16/highs_lows.py
@@ -2,7 +2,7 @@ import csv
 from datetime import datetime
 from matplotlib import pyplot as plt
 
-filename = 'sitka_weather_07-2014.csv'
+filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -21,7 +21,7 @@ with open(filename) as f:
     plt.plot(dates, highs, c='red')
 
     # 设置图形的格式
-    plt.title("Daily high temperatures", fontsize=24)
+    plt.title("Daily high temperatures - 2014", fontsize=24)
     plt.xlabel("", fontsize=16)
     fig.autofmt_xdate()
     plt.ylabel("Temperature (F)", fontsize=16)
